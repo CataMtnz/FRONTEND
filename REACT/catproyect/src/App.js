206dfb28c9a446943625fb81e59b641b71fcc973
CataMtnz <catalinamartinezf3@gmail.com>
2022-07-07 16:54:00 -0400

    commit

---
+++ REACT/catproyect/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <div className="container mt-4">
+            <div className='row'>
+                <div className='col-6'>
+                    <ol>
+                        <li>El Michis</li>
+                        <li>El Cuchos</li>
+                        <li>La Pandilla</li>
+                        <li>Junta</li>
+                        <li>Con Tamales</li>
+                    </ol>
+                </div>
+                <div className='col'>
+                    <h1>Formulario</h1>
+                </div>
+            </div>
+        </div>
+
+    );
+    
+};
+
+export default App;
